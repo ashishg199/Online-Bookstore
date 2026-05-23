5ae626b81ade2744743c60c6e947ade2155a5b2e
Ashish Gupta <ashishji2020@gmail.com>
2026-05-23 17:27:58 +0530

    remove unnecessary files

--- online-bookStore/frontend-2/src/services/api.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = "http://localhost:5000";
-
-export const fetchData = async (url, options = {}) => {
-  const res = await fetch(`${API_URL}${url}`, options);
-  if (!res.ok) throw new Error("API Error");
-  return res.json();
-};
